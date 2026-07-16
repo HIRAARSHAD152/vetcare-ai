b598f1922008de41e80db77d1f4e8dd8ffaf7e22
HIRAARSHAD152 <hiraarshad152@gmail.com>
2026-07-16 19:01:04 +0500

    feat: add password hashing and jwt utilities

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+import env from "../config/env.js";
+
+const generateToken = (payload) => {
+  return jwt.sign(payload, env.JWT_SECRET, {
+    expiresIn: env.JWT_EXPIRES_IN,
+  });
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, env.JWT_SECRET);
+};
+
+export { generateToken, verifyToken };
---
+++ backend/src/utils/password.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs";
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(12);
+
+  return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export { hashPassword, comparePassword };
